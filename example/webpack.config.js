50003889b2e17787c8ec68825bd79a9cdccd54a2
Karissa McKelvey <633012+okdistribute@users.noreply.github.com>
2021-08-16 11:28:52 -0700

    remove unused webpack entry

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -3,12 +3,6 @@ const path = require('path');
 
 module.exports = {
   entry: "./bootstrap.js",
-  resolve: {
-	  fallback: {
-		  
-	  }
-  
-  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",
